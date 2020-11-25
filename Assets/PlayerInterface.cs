2b23c8c261a1e9b2c1b85d7412dd1d41d146fdcd
Andy Phu <malicioussoap@gmail.com>
2020-11-24 22:29:06 -0600

    Recieved an error on fustrum, reloading unity to fix

--- Assets/PlayerInterface.cs
+++ Assets/PlayerInterface.cs
@@ -5,50 +5,29 @@ using UnityEngine;
 public class PlayerInterface : MonoBehaviour
 {
     private float sizeScale = 1;    //sizeScale Factor
-    private GameObject currentGameObject;   //To clean up code
 
-    private float positionY;
-    private float positionX;
-
+    private Camera currentCanera;
     private float mDelta; //Mouse Delta
-
-    //Declare array to store game object positions                                                                                                            ////private float[] initialPositionX = new float[GameObject.FindGameObjectsWithTag("Country").Length];
-    private List<float> initialPositionY = new List<float>();
-
-    private List<float> initialPositionX = new List<float>();
 
     private void Start()
     {
-        for (var i = 0; i < GameObject.FindGameObjectsWithTag("Country").Length; i++) //For the length of game object array, sizeScale each gameobject by mouse defined sizeScale
-        {
-            currentGameObject = GameObject.FindGameObjectsWithTag("Country")[i];
-            initialPositionX.Add(currentGameObject.transform.position.x);
-            initialPositionY.Add(currentGameObject.transform.position.y);
-        }//for
-    }//start
+        currentCanera = Camera.main;
+    }//Void
 
     private void Update()
     {
         if (Input.mouseScrollDelta.y != 0)  //To reduce lag, only check ro try for scaling when relevant
         {
             mDelta = Input.mouseScrollDelta.y;
-            sizeScale += mDelta; // plus or minus change in mouse scroll
+            sizeScale -= mDelta; // plus or minus change in mouse scroll
 
-            if (sizeScale < 3) { sizeScale = 3; } //Caps sizeScale at 3
-            if (sizeScale > 10) { sizeScale = 10; } //Caps sizeScale at 10
+            if (sizeScale < -5) { sizeScale = -5; } //Caps sizeScale at 3
+            if (sizeScale > 0) { sizeScale = 0; } //Caps sizeScale at 10
 
-            //gameObject is the current game Object
-
-            for (var i = 0; i < GameObject.FindGameObjectsWithTag("Country").Length; i++) //For the length of game object array, sizeScale each gameobject by mouse defined sizeScale
-            {
-                currentGameObject = GameObject.FindGameObjectsWithTag("Country")[i];
-                currentGameObject.transform.localScale = new Vector2(sizeScale, sizeScale);
-
-                positionX = currentGameObject.transform.position.x; //Get positions for further manipulation
-                positionY = currentGameObject.transform.position.y; //Get positions for further manipulation
-
-                currentGameObject.transform.localPosition = new Vector2(positionX * mDelta + initialPositionX[i], positionY * mDelta + initialPositionY[i]);        //Do the same with position
-            }//For                                                                                                                                            //gameObject.transform.localsizeScale = new Vector2(sizeScale, sizeScale);
+            currentCanera.orthographicSize = 5 + sizeScale;
         }//If
     }//Void
 }//Class
+
+//Useful Code   // for (var i = 0; i < GameObject.FindGameObjectsWithTag("Country").Length; i++) //For the length of game object array, sizeScale each gameobject by mouse defined sizeScale
+//Useful Code  //gameObject.transform.localsizeScale = new Vector2(sizeScale, sizeScale);
